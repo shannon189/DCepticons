fd1a34fe5c80eecc94a9fdc8735b0cd1a6a772a3
Shannon Myers <shannonemyers189@gmail.com>
2016-01-24 20:48:06 -0500

    pop up for the robots tab, check it out

---
+++ js/popup.js
@@ -0,0 +1,31 @@
+function animationStart(id)
+{
+      window.onwheel = function(){ return false; };
+      document.body.style.overflow = 'hidden';
+
+      var blanket = document.getElementById("blanket");
+      var popUp = document.getElementById(id + "t");
+      var close = document.getElementById(popUp.id + "t");
+      var side = document.getElementById("aside");
+
+      blanket.style.display ="block";
+      popUp.style.display = "block";
+      side.style.width = "215px";
+
+      close.addEventListener("click", function()
+      {
+            blanket.style.display ="none";
+            popUp.style.display = "none";
+            side.style.width = "200px";
+            window.onwheel = function(){ return true; };
+            document.body.style.overflow = 'auto';
+      });
+      blanket.addEventListener("click", function()
+      {
+            blanket.style.display ="none";
+            popUp.style.display = "none";
+            side.style.width = "200px";
+            window.onwheel = function(){ return true; };
+            document.body.style.overflow = 'auto';
+      });
+};
